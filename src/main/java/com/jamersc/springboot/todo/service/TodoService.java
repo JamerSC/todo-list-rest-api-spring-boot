5107a565b3cf6bf66a86de7edce31b0320008967
Jamer Catalla <mr.catalla28@gmail.com>
2025-04-23 08:45:22 +0800

    Updated HTTP Methods for REST CRUD

--- src/main/java/com/jamersc/springboot/todo/service/TodoService.java
+++ src/main/java/com/jamersc/springboot/todo/service/TodoService.java
@@ -6,4 +6,7 @@ import java.util.List;
 
 public interface TodoService {
     List<Todo> findAll();
+    Todo findById(int id);
+    Todo save(Todo todo);
+    void delete(int id);
 }
--- src/main/java/com/jamersc/springboot/todo/service/TodoServiceImpl.java
+++ src/main/java/com/jamersc/springboot/todo/service/TodoServiceImpl.java
@@ -4,11 +4,11 @@ import com.jamersc.springboot.todo.entity.Todo;
 import com.jamersc.springboot.todo.repository.TodoRespository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,5 +21,36 @@ public class TodoServiceImpl implements TodoService {
         List<Todo> todos = todoRespository.findAll();
         todos.forEach(System.out::println);
         return todos;
+    }
+
+    @Override
+    public Todo findById(int id) {
+        // find a todo using optional (collection)
+        Optional<Todo> result = todoRespository.findById(id);
+        // create todo empty object
+        Todo todo = null;
+        if (result.isPresent()) {
+            todo = result.get();
+        } else {
+            throw new RuntimeException("Todo id not found - " + id);
+        }
+        return todo;
+    }
+
+    @Override
+    public Todo save(Todo todo) {
+        return todoRespository.save(todo);
+    }
+
+    @Override
+    public void delete(int id) {
+        Optional<Todo> result = todoRespository.findById(id);
+        Todo todo = null;
+        if (result.isPresent()) {
+            todo = result.get();
+        } else {
+            throw new RuntimeException("Todo id not found - " + id);
+        }
+        todoRespository.delete(todo);
     }
 }
